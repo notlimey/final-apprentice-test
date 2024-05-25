24ed8893f36245306b1455ca84943018c49899e0
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2024-05-25 15:41:30 +0200

    Remove unused files and update Next.js + NextAuth.js starter template

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce9dda3f5c5dfea926b74a5e6a0fcd297e64f21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bcc9b11fadcea2cf889f289e8ed61647b28dfb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,29 @@
-"use client";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
+import { getCurrentSession, getCurrentUser } from '@/common/lib/session';
+import Image from 'next/image';
 
-export default function Home() {
-  const session = useSession();
-  return (
-    <>
-      <div>
-        <h1>
-          Welcome to the <span>Next.js</span> + <span>NextAuth.js</span> starter
-          template!
-        </h1>
-        <pre
-          className="p-4 mt-4 text-sm bg-gray-100 rounded-md"
-        >
-          <code>{JSON.stringify(session, null, 2)}</code>
-        </pre>
-      </div>
-    </>
-  );
+const getRestaurants = async () => {
+	const session = await getCurrentUser();
+
+	if (!session) {
+		return null;
+	}
+
+	return session;
+};
+
+export default async function Home() {
+	const data = await getRestaurants();
+
+	return (
+		<>
+			<div>
+				<h1>
+					Welcome to the <span>Next.js</span> + <span>NextAuth.js</span> starter template!
+				</h1>
+				<pre className='p-4 mt-4 text-sm bg-gray-100 rounded-md'>
+					<code>{JSON.stringify(data, null, 2)}</code>
+				</pre>
+			</div>
+		</>
+	);
 }
