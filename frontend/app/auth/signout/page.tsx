9bcc9b11fadcea2cf889f289e8ed61647b28dfb8
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2024-05-25 15:32:58 +0200

    signout page

---
+++ frontend/app/auth/signout/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Button } from '@/common/components/ui/button';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+export default function Signout() {
+	const router = useRouter();
+
+	const handleSignOut = async () => {
+		const data = await signOut({ redirect: false, callbackUrl: '/' });
+		if (data?.url) {
+			router.push(data.url);
+		}
+
+		router.push('/');
+	};
+
+	return (
+		<div className='flex justify-center items-center min-h-screen w-full'>
+			<Button onClick={() => handleSignOut()}>Sign out</Button>
+		</div>
+	);
+}
