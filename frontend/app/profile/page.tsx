d8b569cea7e8737429d5fe9d626a616ebf7093e7
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2024-05-27 15:09:14 +0200

    profile, extended info

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,7 +1,14 @@
+import { fetchApi } from '@/common/lib/api';
 import { getCurrentSession, getCurrentUser } from '@/common/lib/session';
 
+const getExtendedUser = async () => {
+	const data = await fetchApi('Auth/Personal/Extended');
+
+	return data.data;
+};
+
 export default async function Profile() {
-	const session = await getCurrentSession();
+	const session = await getExtendedUser();
 
 	return (
 		<>
